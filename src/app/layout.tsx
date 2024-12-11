ba47b04eabec9345e14cbb2d35fba1e0fbcf3de0
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-12-11 21:10:17 +0600

    Update layout styles and add Poppins font; enhance homepage content with additional text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/providers/Providers";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Home - Career Dock",
@@ -20,7 +23,7 @@ export default function RootLayout({
   return (
     <Providers>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={poppins.className}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,22 @@ export default function Home() {
   return (
     <main>
       <ModeToggle />
-      <h1 className="text-primary h-5 w-full bg-secondary">
+      <h1 className="text-primary-foreground h-5 w-full bg-primary">
         Welcome to Cateer Dock
       </h1>
+      <p className="text-secondary-foreground">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum facere
+        at autem accusantium atque totam facilis vitae dolorem incidunt
+        doloremque ad, voluptates fugiat ipsam distinctio odio reprehenderit ab
+        et unde repellat minus, sunt blanditiis reiciendis beatae sed. Laborum
+        saepe molestiae quas cumque dolorem fugit iusto, iste delectus est,
+        tempore inventore cupiditate illum quidem explicabo debitis fuga. Enim
+        libero perferendis natus excepturi ipsa. Voluptatem sint incidunt quos,
+        ea ducimus porro similique laboriosam magnam aut non cumque explicabo
+        quibusdam sapiente corrupti tenetur optio nisi molestiae eius? Nemo quia
+        dolor quo eaque est provident modi similique error, itaque voluptatem
+        maxime perferendis! Et, ipsam.
+      </p>
       <Button className="">Click me</Button>
     </main>
   );
