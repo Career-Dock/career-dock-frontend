2d033ebbe683626cfbd21ea777bfb7d2e6aa3ddb
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-12-11 21:06:30 +0600

    Add UI components: AspectRatio, Collapsible, Textarea, Checkbox, Select, Slider, Toaster, and ThemeProvider; update layout and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/providers/Providers";
 import { Toaster } from "react-hot-toast";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,15 @@ export default function RootLayout({
     <Providers>
       <html lang="en">
         <body className={inter.className}>
-          <Toaster />
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </Providers>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { ModeToggle } from "@/components/toggle-dark";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
+      <ModeToggle />
       <h1 className="text-primary h-5 w-full bg-secondary">
-        This is fake 2 new again
+        Welcome to Cateer Dock
       </h1>
+      <Button className="">Click me</Button>
     </main>
   );
 }
