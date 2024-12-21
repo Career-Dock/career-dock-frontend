7513b154cfac46b4098059001fa1aca0693a6ed5
mthtitumir <titumirgccc@gmail.com>
2024-12-21 20:09:39 +0600

    feat: testimonial carosel added

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,6 +1,7 @@
 import FeatureGrid from './_components/landing/feature-grid';
 import FeatureShowcase from './_components/landing/feature-showcase';
 import Hero from './_components/landing/hero';
+import TestimonialCarousel from './_components/landing/testimonial-carousel';
 
 export default async function Home() {
   return (
@@ -8,6 +9,7 @@ export default async function Home() {
       <Hero />
       <FeatureShowcase />
       <FeatureGrid />
+      <TestimonialCarousel />
     </main>
   );
 }
